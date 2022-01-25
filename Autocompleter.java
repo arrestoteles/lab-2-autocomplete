fc53bc59fd3a4f9f30eaec995ff46251b8eb8f66
Christian Sattler <sattler.christian@gmail.com>
2022-01-25 18:56:29 +0100

    Initial commit.

---
+++ Autocompleter.java
@@ -0,0 +1,33 @@
+
+public class Autocompleter {
+    private final Term[] dictionary;
+
+    // Initializes the dictionary from the given array of terms.
+    public Autocompleter(Term[] dictionary) {
+        this.dictionary = dictionary;
+        sortDictionary();
+    }
+
+    // Sorts the dictionary in *case-insensitive* lexicographic order.
+    // Complexity: O(N log N) where N is the number of dictionary terms
+    private void sortDictionary() {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    // Returns the number of terms that start with the given prefix.
+    // Precondition: the internal dictionary is in lexicographic order.
+    // Complexity: O(log N) where N is the number of dictionary terms
+    public int numberOfMatches(String prefix) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    // Returns all terms that start with the given prefix, in descending order of weight.
+    // Precondition: the internal dictionary is in lexicographic order.
+    // Complexity: O(log N + M log M) where M is the number of matching terms
+    public Term[] allMatches(String prefix) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ RangeBinarySearch.java
@@ -0,0 +1,29 @@
+
+import java.util.Comparator;
+
+public class RangeBinarySearch {
+
+    // Returns the index of the *first* element in `a` that equals the search key,
+    // according to the given comparator, or -1 if there is no matching element.
+    // Precondition: `a` is sorted according to the given comparator.
+    // Complexity: O(log N) comparisons where N is the length of `a`
+    public static<T> int firstIndexOf(T[] a, T key, Comparator<T> comparator) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    // Returns the index of the *last* element in `a` that equals the search key,
+    // according to the given comparator, or -1 if there are is matching element.
+    // Precondition: `a` is sorted according to the given comparator.
+    // Complexity: O(log N) comparisons where N is the length of `a`
+    public static<T> int lastIndexOf(T[] a, T key, Comparator<T> comparator) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+
+    // For testing purposes.
+    public static void main(String[] args) {
+        // Here you can write some tests if you want.
+    }
+}
