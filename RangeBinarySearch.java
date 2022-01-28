db704527dcecedeb4964872fe0e84fb909b2df92
Shariq Shahbaz <gusshahbsh@student.gu.se>
2022-01-28 16:11:32 +0100

    finished binary search algorithms

--- RangeBinarySearch.java
+++ RangeBinarySearch.java
@@ -3,6 +3,7 @@ import org.w3c.dom.ranges.Range;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 
 public class RangeBinarySearch {
@@ -12,84 +13,62 @@ public class RangeBinarySearch {
     // Complexity: O(log N) comparisons where N is the length of `a`
     public static<T> int firstIndexOf(T[] a, T key, Comparator<T> comparator) {
         // TODO
-
-
-        int min = 0;
-        int max = a.length - 1;
-
-        while (true) {
-            /*
-            int mid = (lo+hi)/2;
-            if (comparator.compare(a[mid], key) < 0) {        // if true key is in right part of array
-                lo = mid + 1;
-            } else if (comparator.compare(a[mid], key) > 0) {  // if true key is in left part of array
-                hi = mid - 1;
-            } else if (comparator.compare(a[mid], key) == 0) {  // mid == key
-                return mid;
-            }*/
-            int mid = (min + max) / 2;
-            if (min == max) return min;
-            System.out.println("min != max");
-            System.out.println(min+" "+mid+" "+max);
-            if (comparator.compare(a[mid], key) == -1) { // if true key is in right part of array
-                System.out.println("mid < key");
-                min = mid + 1;
-            } else {                                    // if true key is in left part of array
-                System.out.println("hi > key");
-                max = mid;
+        int low = 0;
+        int high = a.length - 1;
+        while (low <= high) {               // Stop when low and high meet
+            if (low == high) return low;
+            int mid = (low + high) / 2;     // Check middle of subarray
+            if (comparator.compare(a[mid], key) < 0) {
+                low = mid + 1;              // In right half
+            } else {
+                high = mid;             // In left half
             }
         }
+        return -1; // Search value not in array
     }
-/*
-        if (min == max) return min;
-        int mid = (lo+hi+1)/2;
-        if (comparator.compare(a[mid], key) == 1) {  // if true key is in right part of array
-            hi = mid - 1;
-        } else {                                    // if true key is in left part of array
-            lo = mid;
-        }
-        return -1;
-    }
-*/
+
     // Returns the index of the *last* element in `a` that equals the search key,
     // according to the given comparator, or -1 if there are is matching element.
     // Precondition: `a` is sorted according to the given comparator.
     // Complexity: O(log N) comparisons where N is the length of `a`
     public static<T> int lastIndexOf(T[] a, T key, Comparator<T> comparator) {
         // TODO
-
-        int lo = a.length-1;
-        int hi = 0;
-        while (lo >= hi) {
-            int mid = (lo+hi)/2;
-            if (comparator.compare(a[mid], key) > 0) {        // if true key is in right part of array
-                lo = mid + 1;
-            } else if (comparator.compare(a[mid], key) < 0) {  // if true key is in left part of array
-                hi = mid - 1;
-            } else if (comparator.compare(a[mid], key) == 0) {  // mid == key
-                return mid;
+        int low = 0;
+        int high = a.length - 1;
+        while (low <= high) {               // Stop when low and high meet
+            if (low == high) return low;
+            int mid = (low + high) / 2;     // Check middle of subarray
+            if (comparator.compare(a[mid], key) > 0) {
+                high = mid;           // In right half
+            } else {
+                low = mid+1; // In left half
             }
         }
-        return -1;
+        return -1; // Search value not in array
     }
 
-
+//answer is 2 and 3 ?
     // For testing purposes.
     public static void main(String[] args) throws IOException {
-        // Here you can write some tests if you want.
-        Comparator<Object> lexiComp = new Comparator<Object>() {
-            public int compare(Object o1, Object o2) {
-                if(o1.getClass() == Term.class && o2.getClass() == Term.class){
-                    Term t1 = (Term) o1;
-                    Term t2 = (Term) o2;
-                    return t1.getWord().compareToIgnoreCase(t2.getWord());
-                }
-                return -8;
-            }
-        };
+            // Here you can write some tests if you want.
+            Term t1 = new Term("abc", 20);
+            Term t2 = new Term("ABCD", 30);
+            Term t3 = new Term("abd", 25);
+            Term t4 = new Term("abd", 15);
+            Term[] terms = {t1, t2, t3, t4};
+            Term[] terms2 = {t1, t3, t2, t4};
 
-        ParsedInput parsedInput = new ParsedInput(new String[]{"/Users/arminbalesic/IdeaProjects/lab-2/dictionaries/gp2011.txt", "340000"});
-        int result = RangeBinarySearch.firstIndexOf(parsedInput.dictionary, "hos", lexiComp);
-        System.out.println(result);
+            ParsedInput parsedInput = new ParsedInput(new String[]{"dictionaries/gp2011.txt", "20000"});
+            Term[] dictionary = parsedInput.dictionary;
+            Arrays.sort(dictionary, Term.byLexicographicOrder);
+            Term key = new Term("Göteborg",0);
+
+
+            System.out.println("First index is: " + firstIndexOf(dictionary,key,Term.byLexicographicOrder));
+            System.out.println("Last index is: " + lastIndexOf(dictionary,key,Term.byLexicographicOrder));
+            /*System.out.println("First index is: " + firstIndexOf(terms,key,Term.byLexicographicOrder));
+            System.out.println("First index is: " + firstIndexOf(terms2,key,Term.byLexicographicOrder));
+            System.out.println("Last index is: " + lastIndexOf(terms,key,Term.byLexicographicOrder));
+            System.out.println("Last index is: " + lastIndexOf(terms2,key,Term.byLexicographicOrder));*/
     }
 }
