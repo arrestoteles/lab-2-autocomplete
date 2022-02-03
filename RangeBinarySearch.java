13b54d322a07bfc2ae3dc32f8a2a08f7931842b0
Shariq Shahbaz <gusshahbsh@student.gu.se>
2022-02-03 17:55:54 +0100

    fixed binary search implementation

--- RangeBinarySearch.java
+++ RangeBinarySearch.java
@@ -13,49 +13,46 @@ public class RangeBinarySearch {
     // Complexity: O(log N) comparisons where N is the length of `a`
     public static<T> int firstIndexOf(T[] a, T key, Comparator<T> comparator) {
         // TODO
-        int low = 0;
-        int high = a.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (low == high && comparator.compare(a[mid], key) == 0){
-                return low;
-            } else if(low == high && comparator.compare(a[mid], key) != 0){
-                break;
-            }
-            if (comparator.compare(a[mid], key) < 0) {
-                low = mid + 1;              // In right half
-            } else {
-                high = mid;             // In left half or the same
-            }
-        }
-        return -1; // Search value not in array
+       int low = 0;
+       int high = a.length - 1;
+       int currentMatch = -1;
+       while (low <= high) {
+           int mid = (low + high) / 2;
+           int comparison = comparator.compare(a[mid], key);
+           if (comparison == 0) {
+               currentMatch = mid;
+               high = mid - 1;              // In right half
+           } else if (comparison < 0) {
+               low = mid+1;
+           } else {
+               high = mid-1;             // In left half or the same
+           }
+       }
+       return currentMatch; // Search value not in array
     }
 
     // Returns the index of the *last* element in `a` that equals the search key,
     // according to the given comparator, or -1 if there are is matching element.
     // Precondition: `a` is sorted according to the given comparator.
     // Complexity: O(log N) comparisons where N is the length of `a`
-    public static<T> int lastIndexOf(T[] a, T key, Comparator<T> comparator) {
+   public static<T> int lastIndexOf(T[] a, T key, Comparator<T> comparator) {
         // TODO
-        int low = 0;
-        int high = a.length-1;
-
-        while (low <= high) {
-
-            int mid = (low + high) / 2;
-            if (low == high && comparator.compare(a[mid], key) == 0){
-                return low;
-            } else if(low == high){
-                break;
-            }
-
-            if (comparator.compare(a[mid], key) <= 0) {
-                low = mid+1;              // In right half
-            } else {
-                high = mid-1;             // In left half or the same
-            }
-        }
-        return -1; // Search value not in array
+       int low = 0;
+       int high = a.length - 1;
+       int currentMatch = -1;
+       while (low <= high) {
+           int mid = (low + high) / 2;
+           int comparison = comparator.compare(a[mid], key);
+           if (comparison == 0) {
+               currentMatch = mid;
+               low = mid+1;              // In right half
+           } else if (comparison < 0) {
+               low = mid+1;
+           } else {
+               high = mid-1;             // In left half or the same
+           }
+       }
+       return currentMatch; // Search value not in array
     }
 
     // 1,3,3,3,4,5,6,7,8,9
@@ -97,10 +94,10 @@ public class RangeBinarySearch {
             Integer[] a = {0, 1};
             Integer[] b = {0, 2};
 
-            /*System.out.println(firstIndexOf(a, 0, Comparator.naturalOrder()));
+            System.out.println(firstIndexOf(a, 0, Comparator.naturalOrder()));
             System.out.println(lastIndexOf(a, 0, Comparator.naturalOrder()));
             System.out.println(firstIndexOf(b, -1, Comparator.naturalOrder()));
-            System.out.println(lastIndexOf(b, -1, Comparator.naturalOrder()));*/
+            System.out.println(lastIndexOf(b, -1, Comparator.naturalOrder()));
 
             /*System.out.println("First index is: " + firstIndexOf(dictionary,key,Term.byLexicographicOrder));
             System.out.println("Last index is: " + lastIndexOf(dictionary,key,Term.byLexicographicOrder));*/
